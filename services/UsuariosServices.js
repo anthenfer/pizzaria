594ed8bf6f183af9e93b9bbd045db91048b7bc16
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2022-12-04 22:52:03 -0300

    Finalizando as funções

--- services/UsuariosServices.js
+++ services/UsuariosServices.js
@@ -117,6 +117,8 @@ function remover(idDoUsuarioParaRemover) {
     //console.log(posicaoUsuario);
 
     //Removendo um usuario do array de usuarios.json  
+    //A função splice recebe dois argumento dentro dos parenteses, o primeiro determina a posição do primeiro item a ser excluído
+    //E o segundo argumento determina o numero de elementos a serem excluídos. 
     usuarios.splice(posicaoUsuario, 1);
     console.log(usuarios);
 
@@ -149,11 +151,11 @@ function addEndereco(novoEndereco, idUsuario) {
 
     //Procurando um usuario com o id recebido
     const usuario = usuarios.find(item => item.id == idUsuario)
-    
+    console.log(usuario);
     //Verificando se existe o usuario
     if(usuario != undefined){
     //Se existe, adicionar novo endereço    
-        novoEndereco.push(usuarios)
+        usuario.endereco.push(novoEndereco)
     } else {
         console.log("Endereço não adicionado")
     }
@@ -169,27 +171,106 @@ function addEndereco(novoEndereco, idUsuario) {
     //Olhar novamente função alterar / detalhar 
 
     //Salvando no arquivo JSON, declarar o array onde será salvo (______,null,4);
-    fs.writeFileSync("./databases/usuarios.json",JSON.stringify(novoEndereco,null,4)); 
+    salvar(usuarios); 
 }
 
 function removerEndereco(posicaoDoEndereco, idUsuario) {
-    // Seu código aqui
+    
+    //Procurando um usuario com o id recebido
+    const usuario = usuarios.find(item => item.id == idUsuario)
+
+    //Aplicando condição
+    if (usuario != undefined){ 
+        usuario.endereco.splice(posicaoDoEndereco, 1)
+    }else {
+        console.log("Usuário não encontrado!")
+    }
+    //Removendo um usuario do array de usuarios.json  
+    //A função splice recebe dois argumento dentro dos parenteses, o primeiro determina a posição do primeiro item a ser excluído
+    //E o segundo argumento determina o numero de elementos a serem excluídos. 
+    //usuario.endereco.splice(posicaoDoEndereco, 1)
+    salvar(usuarios);
 }
 
 function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario) {
-    // Seu código aqui        
+    // Objetivo: A função deve alterar um endereço de um usuário.
+    // Essa função deve receber três parâmetros.
+    // O primeiro, a posicao do endereco no array de enderecos do usuário
+    // O segundo, uma string contendo o novo endereço.
+    // O terceiro, o id do usuário que terá um endereço alterado
+
+    //Procurando um usuario com o id recebido
+    const usuario = usuarios.find(item => item.id == idUsuario)
+    
+    if(usuario != undefined){
+        //Atribuindo um novo endereço para o usuario ' sempre utilizar o sinal de = '
+        usuario.endereco[posicaoDoEndereco] = novoEndereco
+    } else {
+        console.log("Usuário não encontrado")
+    }
+    
+    salvar(usuarios);
 }
 
 function addFormaDePagamento(novaFormaDePagamento, idUsuario) {
-    // Seu código aqui
+    // Objetivo: A função deve adicionar uma nova forma de pagamento no array de formasDePagamento do usuário e atualizar o arquivo usuarios.json
+    // Essa função recebe dois parametros.
+    // O primeiro, uma string contendo a nova forma de pagamento.
+    // O segundo, o id do usuário que terá adicionada a nova forma de pagamento.
+    // Essa função não retorna nada.
+
+    //Procurando um usuario com o id recebido
+    const usuario = usuarios.find(item => item.id == idUsuario)
+
+    //Aplicando condição: adicionando no array 'formasDePagamento' a 'novaFormaDePagamento' 
+    //que a função recebe direto como parametro...
+    if(usuario != undefined){
+        usuario.formasDePagamento.push(novaFormaDePagamento)
+    }else{
+        console.log("Forma de pagamento já existe!")
+    }
+
+    //Chamando a função salvar para atualizar o arquivo usuarios.json
+    salvar(usuarios);
 }
 
 function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario) {
-    // Seu código aqui
+    // Objetivo: A função deve remover a forma de pagamento especificada de um usuário.
+    // Essa função deve receber dois parâmetros.
+    // O primeiro, a posicao da forma de pagamento no array de formas de pagamento do usuário
+    // O segundo, o id do usuário que terá o array removido
+    // Essa função não retorna nada.
+
+    //Procurando um usuario com o id recebido
+    const usuario = usuarios.find(item => item.id == idUsuario)
+    if(usuario != undefined){
+
+        //usuarios referente a 'const = usuarios' que está se referindo 
+    const posicaoDaFormaDePagamentoParaRemover = usuarios.findIndex(item => item.id == posicaoDaFormaDePagamento);
+        //Usando o slice para identificar a posição e remover do array usuarios.json
+    usuarios.splice(posicaoDaFormaDePagamentoParaRemover, 1);
+    
+    } else {
+        console.log("Usuário não encontrado")
+    }
+  
+    //Salvar e atualizar o arquivo usuarios.json
+    salvar(usuarios);
 }
 
 function alterarFormaDePagamento(novaFormaDePagamento, posicaoDaFormaDePagamento, idUsuario) {
-    // Seu código aqui
+    //Encontrando o usuário por ID
+    const usuario = usuarios.find(item => item.id == idUsuario)
+    
+
+    if(usuario != undefined){
+        //Atribuindo um novo endereço para o usuario ' sempre utilizar o sinal de = '
+        usuario.formasDePagamento[posicaoDaFormaDePagamento] = novaFormaDePagamento;
+    } else {
+        console.log("Usuário não encontrado")
+    }
+    
+    salvar(usuarios);
 }
 
 const UsuariosServices = {
--- tests/usuarios.test.js
+++ tests/usuarios.test.js
@@ -2,14 +2,22 @@ const UsuariosServices = require('../services/UsuariosServices.js');
 
 //UsuariosServices.listar();
 
+//UsuariosServices.listarNomes();
+
 //UsuariosServices.salvar([{nome: "Felipe", idade: 51}, {nome: "Maria", idade: 49}]);
+
+//UsuariosServices.cadastrar(novoUsuario); 
+
+//UsuariosServices.cadastrarArray(objeto,array);
+// let novosDados = {
+//         nome: "Marcos da Silva",
+//         email: "marcos@email-do-marcos.com",
+//         senha: "nova-senha"
+//     }
 
 //UsuariosServices.detalhar(129); 
 
-//UsuariosServices.listarNomes();
-
 //UsuariosServices.remover(5);
-
 // let novoUsuario = {
 //         nome: "Maria Cleunice",
 //         email: "email@dousuario.com",
@@ -17,17 +25,17 @@ const UsuariosServices = require('../services/UsuariosServices.js');
 //         endereco: "Rua dos usuários, nº 256. Usuariolândia-BA"
 //     }
 
-//UsuariosServices.cadastrar(novoUsuario); 
-
-//UsuariosServices.cadastrarArray(objeto,array);
-
-// let novosDados = {
-//         nome: "Marcos da Silva",
-//         email: "marcos@email-do-marcos.com",
-//         senha: "nova-senha"
-//     }
-
 //UsuariosServices.alterar(novosDados, 132)
+// let enderecoAdicionado = "Novo Endereço Teste, nº50, Jardim Novo Endereço";
 
-let enderecoAdicionado = ["Novo Endereço Teste, nº50, Jardim Novo Endereço"];
-UsuariosServices.addEndereco(enderecoAdicionado, 132);
+// UsuariosServices.addEndereco(enderecoAdicionado, 132);
+// let novoEndereco = "Novo Endereço, 32 - Bairro do Limoeiro"
+
+//UsuariosServices.removerEndereco(0, 132);
+
+// UsuariosServices.alteraEndereco(0, novoEndereco, 132);
+
+// let novaFormaDePagamento = "Cartão de crédito"
+// UsuariosServices.addFormaDePagamento(novaFormaDePagamento, 130);
+
+UsuariosServices.removerFormaDePagamento(0, 130);
