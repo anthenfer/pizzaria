c791aaece110bff00d71167cbc98c4a1baff0604
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-03-29 11:19:44 -0300

    Fazendo testes com o banco de dados pizzaria.cli

---
+++ tests/conexao.test.js
@@ -0,0 +1,28 @@
+
+//Importando o sequelize
+const sequelize = require('sequelize');
+
+//Importar configurações
+const config = require("../databases/config").development;
+
+//Criar conexão com o banco de dados
+const conexao = new sequelize(config);
+
+//Executar uma consulta 
+const sql = `SELECT
+                p.id,
+                p.nome,
+                ifnull(SUM(pp.quantidade), 0) as quantidade
+            FROM
+                pizzas as p
+                LEFT JOIN pedido_pizza as pp ON p.id = pp.pizza_id
+            group by p.id, p.nome;`
+
+const promise = conexao.query(sql);
+// console.log(promise);
+
+promise.then(
+    data => {
+        console.log(data)
+    }
+);
